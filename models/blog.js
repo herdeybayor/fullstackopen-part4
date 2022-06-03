97f033cfa79f641eabf1bc027fa0b8e3bf2e6c25
herdeybayor <shereefdeen4real@gmail.com>
2022-06-03 08:45:26 +0100

    Exercise 4.11*: Blog list tests, step4 done

--- models/blog.js
+++ models/blog.js
@@ -16,6 +16,7 @@ const blogSchema = new mongoose.Schema({
   },
   likes: {
     type: Number,
+    default: 0,
   },
 });
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -6,9 +6,9 @@ const api = supertest(app);
 
 const Blog = require("../models/blog");
 const helper = require("./test_helper");
-beforeEach(() => {
-  Blog.deleteMany({});
-  Blog.insertMany(helper.initialBlogs);
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
 });
 
 test("blogs are returned in json", async () => {
@@ -19,11 +19,43 @@ test("blogs are returned in json", async () => {
 }, 100000);
 
 test("unique identifier property of the blog posts is named id", async () => {
-  const blogAtStart = await helper.blogsInDb();
-  const sampleBlogId = blogAtStart[0].id;
-  console.log(blogAtStart[0]);
+  const blogsAtStart = await helper.blogsInDb();
+  const sampleBlogId = blogsAtStart[0].id;
 
   expect(sampleBlogId).toBeDefined();
+});
+
+test("a new blog post is created", async () => {
+  const newBlog = {
+    title: "Typescript",
+    author: "Sherifdeen Adebayo",
+    url: "https://sherifdeenadebayo.com",
+    likes: 2,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  const titles = blogsAtEnd.map((blog) => blog.title);
+  expect(titles).toContain("Typescript");
+});
+
+test("a blog with no likes is defaulted to zero", async () => {
+  const blogWithNoLikes = {
+    title: "Blog Dojo",
+    author: "Elon Musk",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+  };
+
+  const response = await api.post("/api/blogs").send(blogWithNoLikes);
+
+  expect(response.body.likes).toBe(0);
 });
 
 afterAll(() => {
